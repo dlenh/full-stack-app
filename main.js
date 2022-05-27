3577f2e036408e7ce24a63853aa4d5370331191b
David Lenh <dlenh425@gmail.com>
2022-05-27 13:38:45 -0700

    Changed bg color

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 document.querySelector("h1").addEventListener("click", changeBg) 
 
 function changeBg() {
-    document.querySelector("body").style.backgroundColor = "teal";
+    document.querySelector("body").style.background = "linear-gradient(to right, #ef5350, #f48fb1, #7e57c2, #2196f3, #26c6da, #43a047, #eeff41, #f9a825, #ff5722)";
 }
